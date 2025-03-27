a083201a4ca3f564a068adb62944c38f24edfecb
Raevyn <106601027+rae-vyn@users.noreply.github.com>
2025-03-27 14:10:38 -0400

    first commit

---
+++ localization/default.lua
@@ -0,0 +1,10 @@
+return {
+    misc = {
+        challenge_names = {
+            c_rchal_splash_i = "Making A Splash I"
+        },
+        v_text = {
+            ch_c_only_high_card = {"The only scoring hand is {C:attention}High Card{}"}
+        }
+    }
+}
---
+++ main.lua
@@ -0,0 +1,56 @@
+---Levels up hand (with proper animation) [from JoyousSpring]
+---@param card Card|table
+---@param hand_key string
+---@param instant boolean?
+---@param amount integer?
+function rae_level_up_hand(card, hand_key, instant, amount)
+    if not instant then
+        update_hand_text({ sound = 'button', volume = 0.7, pitch = 0.8, delay = 0.3 },
+            {
+                handname = localize(hand_key, 'poker_hands'),
+                chips = G.GAME.hands[hand_key].chips,
+                mult = G.GAME.hands[hand_key].mult,
+                level = G.GAME.hands[hand_key].level
+            })
+    end
+    level_up_hand(card, hand_key, instant, amount)
+    if not instant then
+        update_hand_text({ sound = 'button', volume = 0.7, pitch = 1.1, delay = 0 },
+            { mult = 0, chips = 0, handname = '', level = '' })
+    end
+end
+
+local evaluate_poker_hand_ref = evaluate_poker_hand
+function evaluate_poker_hand(hand)
+    local ret = evaluate_poker_hand_ref(hand)
+    if G.GAME.modifiers.only_high_card == true then
+        for k, v in pairs(ret) do
+            if k ~= "High Card" then
+                ret[k] = {}
+            end
+        end
+    end
+    return ret
+end
+
+SMODS.Challenge({
+    key = "splash_i",
+    jokers = {
+        {id = "j_splash", edition = "negative", eternal = true}
+    },
+    rules = {
+        custom = {
+            {id = "only_high_card"}
+        }
+    },
+    restrictions = {
+        banned_other = {{id = "bl_eye", type = "blind"}, {id = "bl_ox", type = "blind"}}
+    }
+})
+
+SMODS.Keybind({
+    key_pressed = "delete",
+    action = function (self)
+        SMODS.restart_game()
+    end
+})
