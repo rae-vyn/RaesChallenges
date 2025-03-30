5b4bdbff067b180fa852a8b469f32d9e826a0a4c
Raevyn <106601027+rae-vyn@users.noreply.github.com>
2025-03-30 09:28:56 -0400

    finished all the challenges (for now)

--- localization/default.lua
+++ localization/default.lua
@@ -1,17 +1,30 @@
 return {
     misc = {
         challenge_names = {
-            c_rchal_splash_i = "Making A Splash I",
+            c_rchal_splash_i = "Making A Splash",
             c_rchal_splash_ii = "Making A Splash II",
             c_rchal_splash_iii = "Making A Splash III",
             c_rchal_splash_light = "Splash: Light Work",
             c_rchal_truthers = "For The Truthers",
+            c_rchal_truthers_ii = "For The Truthers II",
+            c_rchal_truthers_iii = "You Can't Handle The Truth",
+            c_rchal_monopoly_mult = "Monopoly Mult",
+            c_rchal_spaced_out = "Spaced Out",
+            c_rchal_chip_in = "Chip-In",
+            c_rchal_mult_mogul = "Mult Mogul",
+            c_rchal_pair_to_win = "P2W",
+            c_rchal_economist = "The Economist"
         },
         v_text = {
-            ch_c_only_hand = {"The only scoring hand is {C:attention}#1#{}"},
+            ch_c_only_hand = {"The only existing hand is {C:attention}#1#{}"},
             ch_c_hand_level = {"{C:attention}#1#{} starts at level {C:mult}#2#"},
             ch_c_faceless = {"Start with no {C:attention}Face Cards{}"},
-            ch_c_harold = {"{C:money}Star{} could solo this."}
+            ch_c_harold = {"Inspired by {C:money}Star{} in the Balatro server"},
+            ch_c_mult_dollar_cap = {"{C:mult}Mult{} cannot exceed {C:attention}2X{} current {C:money}$"},
+            ch_c_mult_chip_cap = {"{C:mult}Mult{} cannot exceed current {C:blue}Chips"},
+            ch_c_no_mult = {"{C:mult}Mult{} is capped at {C:mult}1"},
+            ch_c_pair_tax = {"Every played {C:blue}Pair{} costs {C:money}$2"},
+            ch_c_all_rental = {"Every Joker is a {C:money}Rental"}
         }
     }
 }
--- main.lua
+++ main.lua
@@ -4,60 +4,155 @@
 ---@param instant boolean?
 ---@param amount integer?
 function rae_level_up_hand(card, hand_key, instant, amount)
-    if not instant then
-        update_hand_text({ sound = 'button', volume = 0.7, pitch = 0.8, delay = 0.3 },
-            {
-                handname = localize(hand_key, 'poker_hands'),
-                chips = G.GAME.hands[hand_key].chips,
-                mult = G.GAME.hands[hand_key].mult,
-                level = G.GAME.hands[hand_key].level
-            })
-    end
-    level_up_hand(card, hand_key, instant, amount)
-    if not instant then
-        update_hand_text({ sound = 'button', volume = 0.7, pitch = 1.1, delay = 0 },
-            { mult = 0, chips = 0, handname = '', level = '' })
-    end
+	if not instant then
+		update_hand_text({ sound = "button", volume = 0.7, pitch = 0.8, delay = 0.3 }, {
+			handname = localize(hand_key, "poker_hands"),
+			chips = G.GAME.hands[hand_key].chips,
+			mult = G.GAME.hands[hand_key].mult,
+			level = G.GAME.hands[hand_key].level,
+		})
+	end
+	level_up_hand(card, hand_key, instant, amount)
+	if not instant then
+		update_hand_text(
+			{ sound = "button", volume = 0.7, pitch = 1.1, delay = 0 },
+			{ mult = 0, chips = 0, handname = "", level = "" }
+		)
+	end
 end
 
 function values(tbl)
-    local vals = {};
-    for _, v in pairs(tbl) do
-        vals[#vals+1] = v
-    end
+	local vals = {}
+	for _, v in pairs(tbl) do
+		vals[#vals + 1] = v
+	end
 
-    return vals
+	return vals
+end
+
+function mod_mult(_mult)
+	if G.GAME.modifiers.mult_dollar_cap then
+		return math.min(_mult, math.max(2 * G.GAME.dollars, 0))
+	elseif G.GAME.modifiers.no_mult then
+		return 1
+	elseif G.GAME.modifiers.mult_chip_cap then
+		return math.min(_mult, hand_chips)
+	end
+	return _mult
 end
 
 local evaluate_poker_hand_ref = evaluate_poker_hand
 function evaluate_poker_hand(hand)
-    local ret = evaluate_poker_hand_ref(hand)
-    if G.GAME.modifiers.only_hand then
-        for k, v in pairs(ret) do
-            if (k ~= G.GAME.modifiers.only_hand and k ~= "High Card") then
-                ret[k] = {}
-            end
-        end
-    end
-    return ret
+	local ret = evaluate_poker_hand_ref(hand)
+	if G.GAME.modifiers.only_hand then
+		for k, v in pairs(ret) do
+			if (k ~= "High Card") and (k ~= G.GAME.modifiers.only_hand) then
+				ret[k] = {}
+			end
+		end
+	end
+	return ret
 end
 
-SMODS.Challenge({
-    key = "splash_i",
-    jokers = {
-        {id = "j_splash", edition = "negative", eternal = true}
-    },
-    rules = {
-        custom = {
-            {id = "hand_level", value = {hand = "High Card", level = 6}},
-            {id = "only_high_card"},
-        }
-    },
-    restrictions = {
-        banned_other = {{id = "bl_eye", type = "blind"}, {id = "bl_ox", type = "blind"}}
-    }
+SMODS.Challenge({ -- Splash I
+	key = "splash_i",
+	jokers = {
+		{ id = "j_splash", edition = "negative", eternal = true },
+	},
+	rules = {
+		custom = {
+			{ id = "hand_level", value = { hand = "High Card", level = 6 } },
+			{ id = "only_hand", value = "High Card" },
+		},
+	},
+	restrictions = {
+		banned_other = { { id = "bl_eye", type = "blind" }, { id = "bl_ox", type = "blind" } },
+	},
 })
 
+SMODS.Challenge({ -- Mono Mult
+	key = "monopoly_mult",
+	vouchers = {
+		{ id = "v_directors_cut" },
+	},
+	rules = {
+		custom = {
+			{ id = "mult_dollar_cap" },
+		},
+		modifiers = {
+			{ id = "dollars", value = 30 },
+		},
+	},
+	restrictions = {
+		banned_other = { { id = "bl_ox", type = "blind" } },
+	},
+})
+
+SMODS.Challenge({ -- Spaceman
+	key = "spaced_out",
+	jokers = {
+		{ id = "j_space", eternal = true },
+		{ id = "j_oops" },
+	},
+	rules = {
+		custom = {
+			{ id = "no_shop_jokers" },
+		},
+	},
+	restrictions = {
+		banned_cards = {
+			{ id = "c_mercury" },
+			{ id = "c_venus" },
+			{ id = "c_earth" },
+			{ id = "c_mars" },
+			{ id = "c_jupiter" },
+			{ id = "c_saturn" },
+			{ id = "c_uranus" },
+			{ id = "c_neptune" },
+			{ id = "c_eris" },
+			{ id = "c_ceres" },
+			{ id = "c_planet_x" },
+			{
+				id = "p_celestial_normal_1",
+				ids = {
+					"p_celestial_normal_2",
+					"p_celestial_normal_3",
+					"p_celestial_normal_4",
+					"p_celestial_jumbo_1",
+					"p_celestial_jumbo_2",
+					"p_celestial_mega_1",
+					"p_celestial_mega_2",
+				},
+			},
+			{ id = "c_high_priestess" },
+			{ id = "j_burnt" },
+		},
+		banned_tags = {
+			{ id = "tag_meteor" },
+		},
+		banned_other = { { id = "bl_ox", type = "blind" } },
+	},
+})
+
+SMODS.Challenge({ -- Chips
+	key = "chip_in",
+	jokers = {
+		{ id = "j_blue_joker", edition = "foil" },
+	},
+	rules = {
+		custom = {
+			{ id = "no_mult" },
+		},
+	},
+	restrictions = {
+		banned_other = { { id = "bl_ox", type = "blind" } },
+	},
+	deck = {
+		type = "Challenge Deck",
+		enhancement = "m_bonus",
+	},
+})
+--[[
 SMODS.Challenge({
     key = "splash_ii",
     jokers = {
@@ -105,70 +200,216 @@ SMODS.Challenge({
             J = true
         }
     }
+})]]
+
+SMODS.Challenge({ -- Truthers
+	key = "truthers",
+	jokers = {
+		{ id = "j_tribe", edition = "negative", eternal = true },
+		{ id = "j_devious" },
+	},
+	rules = {
+		custom = {
+			{ id = "only_hand", value = "Straight Flush" },
+			{ id = "faceless" },
+		},
+		modifiers = {
+			{ id = "hand_size", value = 10 },
+		},
+	},
+	restrictions = {
+		banned_other = { { id = "bl_eye", type = "blind" }, { id = "bl_ox", type = "blind" } },
+	},
+	deck = {
+		type = "Challenge Deck",
+		no_ranks = {
+			K = true,
+			Q = true,
+			J = true,
+		},
+	},
 })
 
-SMODS.Challenge({
-    key = "splash_light",
-    jokers = {
-        {id = "j_splash", edition = "negative", eternal = true}
-    },
-    rules = {
-        custom = {
-            {id = "hand_level", value = {hand = "High Card", level = 5}},
-            {id = "only_hand", value = "High Card"},
-            {id = "faceless"},
-            {id = "harold"}
-        },
-        modifiers = {
-            {id = "hands", value = 1},
-            {id = "discards", value = 1},
-            {id = "hand_size", value = 3}
-        }
-    },
-    restrictions = {
-        banned_other = {{id = "bl_eye", type = "blind"}, {id = "bl_ox", type = "blind"}}
-    },
-    deck = {
-        type = "Challenge Deck",
-        no_ranks = {
-            K = true,
-            Q = true,
-            J = true
-        }
-    }
+SMODS.Challenge({ -- Mult Mogul
+	key = "mult_mogul",
+	vouchers = {
+		{ id = "v_directors_cut" },
+	},
+	rules = {
+		custom = {
+			{ id = "mult_chip_cap" },
+		},
+	},
+	restrictions = {
+		banned_other = { { id = "bl_ox", type = "blind" }, { id = "bl_flint", type = "blind" } },
+	},
 })
 
-SMODS.Challenge({
-    key = "truthers",
-    jokers = {
-        {id = "j_tribe", edition = "negative", eternal = true},
-        {id = "j_devious"},
-    },
-    rules = {
-        custom = {
-            {id = "only_hand", value = "Straight Flush"},
-            {id = "faceless"},
-        },
+SMODS.Challenge({ -- P2W
+	key = "pair_to_win",
+	rules = {
+		custom = {
+            { id = "only_hand", value = "Pair" },
+            { id = "hand_level", value = {hand = "Pair", level = 6}},
+			{ id = "pair_tax" },
+		},
         modifiers = {
-            {id = "hand_size", value = 10}
-        }
-    },
-    restrictions = {
-        banned_other = {{id = "bl_eye", type = "blind"}, {id = "bl_ox", type = "blind"}}
-    },
-    deck = {
-        type = "Challenge Deck",
-        no_ranks = {
-            K = true,
-            Q = true,
-            J = true
-        }
-    }
+            { id = "dollars", value = 22 }
+        },
+	},
+	restrictions = {
+		banned_other = { { id = "bl_ox", type = "blind" }, { id = "bl_club", type = "blind" }, { id = "bl_window", type = "blind" },},
+	},
+	deck = {
+		type = "Challenge Deck",
+		cards = { -- Pairs of Diamonds and Clubs
+			{ s = "D", r = "2" },
+			{ s = "D", r = "3" },
+			{ s = "D", r = "4" },
+			{ s = "D", r = "5" },
+			{ s = "D", r = "6" },
+			{ s = "D", r = "7" },
+			{ s = "D", r = "8" },
+			{ s = "D", r = "9" },
+			{ s = "D", r = "T" },
+			{ s = "D", r = "J" },
+			{ s = "D", r = "Q" },
+			{ s = "D", r = "K" },
+			{ s = "D", r = "A" },
+			{ s = "C", r = "2" },
+			{ s = "C", r = "3" },
+			{ s = "C", r = "4" },
+			{ s = "C", r = "5" },
+			{ s = "C", r = "6" },
+			{ s = "C", r = "7" },
+			{ s = "C", r = "8" },
+			{ s = "C", r = "9" },
+			{ s = "C", r = "T" },
+			{ s = "C", r = "J" },
+			{ s = "C", r = "Q" },
+			{ s = "C", r = "K" },
+			{ s = "C", r = "A" },
+			{ s = "D", r = "2" },
+			{ s = "D", r = "3" },
+			{ s = "D", r = "4" },
+			{ s = "D", r = "5" },
+			{ s = "D", r = "6" },
+			{ s = "D", r = "7" },
+			{ s = "D", r = "8" },
+			{ s = "D", r = "9" },
+			{ s = "D", r = "T" },
+			{ s = "D", r = "J" },
+			{ s = "D", r = "Q" },
+			{ s = "D", r = "K" },
+			{ s = "D", r = "A" },
+			{ s = "C", r = "2" },
+			{ s = "C", r = "3" },
+			{ s = "C", r = "4" },
+			{ s = "C", r = "5" },
+			{ s = "C", r = "6" },
+			{ s = "C", r = "7" },
+			{ s = "C", r = "8" },
+			{ s = "C", r = "9" },
+			{ s = "C", r = "T" },
+			{ s = "C", r = "J" },
+			{ s = "C", r = "Q" },
+			{ s = "C", r = "K" },
+			{ s = "C", r = "A" },
+		},
+	},
+})
+
+SMODS.Challenge({ -- Economist
+	key = "economist",
+	vouchers = {
+		{ id = "v_seed_money" },
+        { id = "v_money_tree" },
+	},
+	rules = {
+		custom = {
+			{ id = "all_rental" },
+		},
+        modifiers = {
+            { id = "dollars", value = 25 }
+        },
+	},
+})
+
+SMODS.Challenge({ -- Splash II
+	key = "splash_light",
+	jokers = {
+		{ id = "j_splash", edition = "negative", eternal = true },
+	},
+	rules = {
+		custom = {
+			{ id = "hand_level", value = { hand = "High Card", level = 5 } },
+			{ id = "only_hand", value = "High Card" },
+			{ id = "faceless" },
+			{ id = "harold" },
+		},
+		modifiers = {
+			{ id = "hands", value = 2 },
+			{ id = "discards", value = 1 },
+			{ id = "hand_size", value = 3 },
+		},
+	},
+	restrictions = {
+		banned_other = { { id = "bl_eye", type = "blind" }, { id = "bl_ox", type = "blind" } },
+	},
+	deck = {
+		type = "Challenge Deck",
+		no_ranks = {
+			K = true,
+			Q = true,
+			J = true,
+		},
+	},
+})
+
+SMODS.Challenge({ -- Truthers II
+	key = "truthers_ii",
+	jokers = {
+		{ id = "j_tribe", edition = "negative", eternal = true },
+		{ id = "j_crafty" },
+	},
+	rules = {
+		custom = {
+			{ id = "only_hand", value = "Flush" },
+			{ id = "faceless" },
+		},
+		modifiers = {
+			{ id = "hand_size", value = 10 },
+		},
+	},
+	restrictions = {
+		banned_cards = {
+			{ id = "j_greedy_joker" },
+			{ id = "j_lusty_joker" },
+			{ id = "j_wrathful_joker" },
+			{ id = "j_gluttenous_joker" },
+			{ id = "j_droll" },
+			{ id = "j_crafty" },
+			{ id = "j_smeared" },
+			{ id = "j_rough_gem" },
+			{ id = "j_bloodstone" },
+			{ id = "j_arrowhead" },
+			{ id = "j_onyx_agate" },
+		},
+		banned_other = { { id = "bl_eye", type = "blind" }, { id = "bl_ox", type = "blind" } },
+	},
+	deck = {
+		type = "Challenge Deck",
+		no_ranks = {
+			K = true,
+			Q = true,
+			J = true,
+		},
+	},
 })
 
 SMODS.Keybind({
-    key_pressed = "delete",
-    action = function (self)
-        SMODS.restart_game()
-    end
+	key_pressed = "delete",
+	action = function(self)
+		SMODS.restart_game()
+	end,
 })
